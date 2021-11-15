5b245afb4282bc17698417fd374f789cce156981
unknown <briankingDCC@gmail.com>
2021-11-15 16:14:34 -0500

    finished exercise 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css"
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import DisplayName from "./components/DisplayName/DisplayName.jsx"
+// import AlertUser from "./components/AlertUser/AlertUser.jsx"
+// import NamesList from "./components/NamesList/NamesList.jsx"
+// import SuperHeroTable from "./components/SuperHeroTable/SuperHeroTable.jsx"
+
+// 
+
+class App extends Component {
+
+    state = {
+        firstName: prompt("Whats your first name ??"),
+        lastName: prompt("Whats your Last name ??")
+    }
+
+    render(){
+        return (
+            <div className="App">
+                <h1>React Components Worksheet</h1>
+                <h2>Import relevant component and add seed data if needed</h2>
+                <DisplayName
+                firstName={this.state.firstName}
+                lastName={this.state.lastName} />
+            </div>
+        );
+    }
 }
 
 export default App;
